19b1015af1201d791b53d5d775c7449d016157a1
Gabriela RB <gabrielaruizblake@gmail.com>
2019-07-19 23:36:43 -0500

    details added to the navbar

--- src/componenets/navbar/navbar.js
+++ src/componenets/navbar/navbar.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "./navbar.css";
 import { Navbar } from "react-bootstrap";
-import Jumbotron from 'react-bootstrap/Jumbotron';
-
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 const header = props => (
   <>
-    <Navbar className="navbar" expand="lg" bg="light"> 
-      <Navbar.Brand className="title" href="/">
-        {props.children}
-      </Navbar.Brand>
-      <Navbar.Collapse className="justify-content-end"></Navbar.Collapse>
-      <Navbar.Toggle />
-      <Navbar.Text className="scores">
-        Score: {props.score} Highscore: {props.highscore}
-      </Navbar.Text>
+    <Navbar className="navbar" expand="lg" bg="light">
+      <div class="container-fluid">
+        <Navbar.Brand className="title" href="/">
+          {props.children}
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Text className="correct">
+          {props.correctness}
+        </Navbar.Text>
+        <Navbar.Toggle />
+        <Navbar.Text className="scores">
+          Score: {props.score} | Highscore: {props.highscore}
+        </Navbar.Text>
+      </div>
     </Navbar>
-    <Jumbotron className="header" >
+    <Jumbotron className="header">
       <h1>Clicky Game!</h1>
       <h2>
         Click on an image to earn points, but don't click on any more than once!
