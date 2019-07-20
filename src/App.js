d886dcb911d79763f055963ea143625b0ba74f62
Gabriela RB <gabrielaruizblake@gmail.com>
2019-07-19 19:08:45 -0500

    bootstrap added to navbar.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import Card from "./componenets/card/card";
 import Navbar from "./componenets/navbar/navbar";
--- src/componenets/navbar/navbar.js
+++ src/componenets/navbar/navbar.js
@@ -1,13 +1,17 @@
 import React from "react";
-import "./navbar.css"
+//import "./navbar.css";
+//import { navbar } from "react-bootstrap";
 
 const Navbar = props => (
-    <nav className="navbar">
-    <a className="title" href="/">{props.children}</a>
-    <div className="scores my-sm-0">
+  <Navbar className="navbar">
+    <Navbar.Brand className="title" href="/">
+      {props.children}
+    </Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.text className="scores mr-auto">
       Score: {props.score} Highscore: {props.highscore}
-    </div>
-  </nav>
+    </Navbar.text>
+  </Navbar>
 );
 
 export default Navbar;
