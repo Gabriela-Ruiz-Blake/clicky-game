08e8fd73125aafb8036d1d89b321436ca4c8482d
Gabriela RB <gabrielaruizblake@gmail.com>
2019-07-19 20:57:08 -0500

    boostrap added successfully

--- src/componenets/navbar/navbar.js
+++ src/componenets/navbar/navbar.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "./navbar.css";
-import { navbar } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
-const Navbar = props => (
-  <nav className="navbar">
-    <a className="title" href="/">
-      {props.children}
-    </a>
-    
-    <div className="scores mr-auto">
-      Score: {props.score} Highscore: {props.highscore}
-    </div>
-  </nav>
+
+const header = props => (
+  <>
+    <Navbar className="navbar" expand="lg" bg="light"> 
+      <Navbar.Brand className="title" href="/">
+        {props.children}
+      </Navbar.Brand>
+      <Navbar.Collapse className="justify-content-end"></Navbar.Collapse>
+      <Navbar.Toggle />
+      <Navbar.Text className="scores">
+        Score: {props.score} Highscore: {props.highscore}
+      </Navbar.Text>
+    </Navbar>
+    <Jumbotron className="header" >
+      <h1>Clicky Game!</h1>
+      <h2>
+        Click on an image to earn points, but don't click on any more than once!
+      </h2>
+    </Jumbotron>
+  </>
 );
 
-export default Navbar;
+export default header;
